e4388d9cb040a63619d12d1c8f61a567105c031e
jeerawat_k <>
2019-02-08 15:38:00 +0700

    Create first component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { ProductsComponent } from './products/products.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductsComponent },
+  { path: 'product/:id', component: ProductComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
